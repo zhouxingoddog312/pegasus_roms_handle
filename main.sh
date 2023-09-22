ea14fd80b5ce5d94264734d2b247cf8720aa9293
zhouxin <zhouxin312@qq.com>
2023-09-22 12:53:18 +0800

    modify generate database

--- main.sh
+++ main.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
-#depend on sed,7z
+#depend on sed,7z,tofrodos,imagemagick
 CUR_DIR=`pwd`
 SOURCE_DIR=$CUR_DIR/"source"
 TRANSF_DIR=$CUR_DIR/"transf"
 TARGET_DIR=$CUR_DIR/"target"
+LOG=$CUR_DIR/"logfile"
 METADATA_TITLE=$SOURCE_DIR/"metadata_title"
 METADATA_LAUNCH=$SOURCE_DIR/"metadata_launch"
 METADATA_GAMES=$SOURCE_DIR/"metadata_games"
-ROMS_LIST=$SOURCE_DIR/"roms_list"
-declare -A game_roms
-declare -A game_medias
+declare -A games_roms
+declare -A games_medias
 source ./functions
 version
 help
 haswkdir
-#check_source
+check_source
 generate_database $METADATA_GAMES

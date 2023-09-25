b03b7072be15008fe26edd7306a63bcf3c20bc07
zhouxin <zhouxin312@qq.com>
2023-09-25 21:56:40 +0800

    change  to

--- main.sh
+++ main.sh
@@ -4,10 +4,12 @@ CUR_DIR=`pwd`
 SOURCE_DIR=$CUR_DIR/"source"
 TRANSF_DIR=$CUR_DIR/"transf"
 TARGET_DIR=$CUR_DIR/"target"
+TARGET_ROM=""
+TARGET_MEDIA=""
 LOG=$CUR_DIR/"logfile"
-METADATA_TITLE=$SOURCE_DIR/"metadata_title"
-METADATA_LAUNCH=$SOURCE_DIR/"metadata_launch"
-METADATA_GAMES=$SOURCE_DIR/"metadata_games"
+METADATA_TITLE=$TRANSF_DIR/"metadata_title"
+METADATA_LAUNCH=$TRANSF_DIR/"metadata_launch"
+METADATA_GAMES=$TRANSF_DIR/"metadata_games"
 declare -A games_roms
 declare -A games_medias
 declare -A games_descriptions
@@ -18,10 +20,10 @@ haswkdir
 #check_source
 generate_database $METADATA_GAMES
 #################测试
-str=$(select_interface)
-OLDIFS=$IFS
-IFS='|'
-declare -a array
-read -ra array <<<"$str"
-echo ${array[@]}
+#str=$(select_interface)
+#OLDIFS=$IFS
+#IFS='|'
+#declare -a array
+#read -ra array <<<"$str"
+#echo ${array[@]}
 ###################

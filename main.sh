769be3c9759b9b5faf7934cb88f871c30134a0fc
zhouxin <zhouxin312@qq.com>
2023-09-29 11:54:56 +0800

    由main创建log文件，清理工作由main调用clean处理

--- main.sh
+++ main.sh
@@ -9,8 +9,12 @@ ROMS_SUB_DIRNAME=""
 METADATA_TITLE=$TRANSF_DIR/"metadata_title"
 METADATA_LAUNCH=$TRANSF_DIR/"metadata_launch"
 METADATA_GAMES=$TRANSF_DIR/"metadata_games"
-declare -i WIDTH=$[ $COLUMNS * 3 ]
-declare -i HEIGHT=$[ $LINES * 3 ]
+
+exec 4>&2
+exec 2>"$LOG"
+
+declare -i WIDTH=$[ COLUMNS * 3 ]
+declare -i HEIGHT=$[ LINES * 3 ]
 declare -A games_roms
 declare -A games_medias
 declare -A games_descriptions
@@ -24,3 +28,5 @@ generate_database $METADATA_GAMES
 select_result
 check_target
 move_files
+
+exec 2>&4

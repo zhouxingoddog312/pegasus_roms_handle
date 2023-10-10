f5e241050bbba16c168728d1fb9c96b2c2ca5034
zhouxin <zhouxin312@qq.com>
2023-10-10 17:16:15 +0800

    add install functions

--- main.sh
+++ main.sh
@@ -1,26 +1,39 @@
 #! /bin/bash
 #depend on sed,7z,unix2dos,zenity
+##############环境变量
+SCRIPT_DIR=$(dirname "$0")
+LIB_PATH="$SCRIPT_DIR/functions"
+METADATA_DIR=$SCRIPT_DIR/"src/metadata"
 CUR_DIR=`pwd`
 SOURCE_DIR=$CUR_DIR/"source"
 TRANSF_DIR=$CUR_DIR/"transf"
 TARGET_DIR=$CUR_DIR/"target"
-METADATA_DIR=$CUR_DIR/"src/metadata"
 LOG=$CUR_DIR/"logfile"
 ROMS_SUB_DIRNAME=""
 METADATA_TITLE=$TRANSF_DIR/"metadata_title"
 METADATA_LAUNCH=$TRANSF_DIR/"metadata_launch"
 METADATA_GAMES=$TRANSF_DIR/"metadata_games"
-
 exec 4>&2
 exec 2>"$LOG"
-
-declare -i WIDTH=$[ COLUMNS * 10 ]
-declare -i HEIGHT=$[ LINES * 10 ]
+declare -i WIDTH=$[ COLUMNS * 2 ]
+declare -i HEIGHT=$[ LINES * 2 ]
 declare -A games_roms
 declare -A games_medias
 declare -A games_descriptions
 declare -a games_selected
-source ./functions
+#################################
+
+###函数库
+source "$LIB_PATH"
+############
+
+#############安装依赖
+install_sed
+install_dos2unix
+install_7z
+install_zenity
+##############
+
 version
 help
 haswkdir
